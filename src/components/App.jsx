96cc5a22005ce26f2fe5568840c4c94ae40d1285
oleksandr-vasylenko <oleksandr.vasylenko.ua@gmail.com>
2022-12-02 20:20:32 +0200

    Sets up entire App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,34 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import { FeedbackOptions } from './FeedbackOptions';
+import { Statistics } from './Statistics';
+
+export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  render() {
+    return (
+      <section>
+        <h1>Please Leave Feedback</h1>
+
+        <div>
+          <ul>
+            <FeedbackOptions />
+          </ul>
+        </div>
+
+        <div>
+          <h2>Statistics</h2>
+          <Statistics />
+        </div>
+      </section>
+    );
+  }
+
+  // countTotalFeedback();
+
+  // countPositiveFeedbackPercentage();
+}
